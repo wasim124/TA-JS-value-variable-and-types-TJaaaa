4b1a35f82e2bb785d0947671719db5a74c979d7a
wasimul haque <mailofwasim@gmail.com>
2022-04-13 15:58:11 +0530

    js

--- block-BJaabg/code/index.js
+++ block-BJaabg/code/index.js
@@ -15,4 +15,23 @@ var bank_balance = 303.91;
 var amount = 0;
 // your code goes here
 
+while(amount < bank_balance){
+    amount +=  PHONE_PRICE;
+    if(amount < SPENDING_THRESHOLD) {
+        amount +=ACCESSORY_PRICE;
+    }
+}
+
+let tax = amount * TAX_RATE;
+
+let taxedAmount = amount + tax;
+
+console.log(`$ ${taxedAmount}`);
+
+if (taxedAmount < bank_balance){
+   console.log(`you can afford it`) 
+}else{
+    console.log(`you can't afford it`)
+}
+
 // ⛑ Answer of the above will `$334.76`.
