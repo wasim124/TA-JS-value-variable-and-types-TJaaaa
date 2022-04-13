38d9ff7d76ea09de619e92b7ec09024b20588821
wasimul haque <mailofwasim@gmail.com>
2022-04-13 14:03:01 +0530

    js

--- block-BJaabb/code/index.js
+++ block-BJaabb/code/index.js
@@ -6,21 +6,65 @@
 4. Now change the value of `user` to `John` and log the value of both `user` and `userName`.
 */
 
+let user = prompt("Enter your name?");
+
+alert(user);
+let userName = user;
+
+console.log(user, userName);
+let age = 22, isLearning = true;
+
+user = "Jhon";
+
+console.log(user, userName);
+
+
 /* 2.
 Use `prompt` to take two numbers from user and store it in variable `numA` and `numB`. Create a third variable called `sum`, using `+` operator add numA and numB , store it in varible `sum` and using alert display `sum` in browser.
 */
+
+let numA = Number(prompt("enter 1st number"));
+let numB = Number(prompt("enter 2nd number"));
+let sum = numA + numB;
+alert(sum);
 
 // Loops
 
 // 3. Complete the following code to make the output be 0 2 4 6 8 10?
 
-for (let j = 0; j <= 10; ____) console.log(j);
+for (let j = 0; j <= 10; j += 2) { console.log(j); }
 
 // 4. Print the odd numbers from 9 to 1 (both inclusive) using a for loop.
 
+for (let i = 9; i >= 1; i--) {
+    if (i % 2 !== 0) {
+        console.log(i`odd number`);
+    }
+}
+
 // 5. Concat all the numbers from 5 to 0 (both inclusive) in descending order using a while loop. (543210)
 
+let finalString = "";
+let initialValue = 5;
+
+while (initialValue >= 0) {
+    finalString += initialValue;
+    initialValue--;
+}
+console.log(finalString);
+
 // 6. Concat all the even numbers from 10 to 0 (both inclusive) in descending order using a while loop. (1086420)
+
+let finalEvenString = "";
+let initialEvenValue = 10;
+
+while (initialEvenValue >= 0) {
+    if (initialEvenValue % 2 === 0) {
+        finalEvenString += initialEvenValue;
+    }
+    initialEvenValue--;
+}
+console.log(finalEvenString);
 
 // Comparisoin
 
@@ -36,3 +80,12 @@ Example:
 "hello", -21; // false
 
 */
+
+let num1 = +prompt("Enter num one");
+let num2 = +prompt("Enter num one");
+
+if(isNaN(num1) || isNaN(num2)){
+    alert("Enter a valid value");
+} else {
+    alert(num1 === num2);
+}
